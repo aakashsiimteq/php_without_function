5e12d7d40d8f8da11b084d07871b17386407fda9
Aakash Dhar <aakash.dhar@simteq.com>
2018-02-01 12:41:48 +0530

    unique array using manual method

---
+++ array_unique.php
@@ -0,0 +1,15 @@
+<?php
+
+// deleting multiple records in an array
+$inputArray = array(1, 4, 2, 1, 6, 4, 9, 7, 2, 9);
+$outputArray = array();
+
+foreach($inputArray as $inputArrayItem) {
+  foreach($outputArray as $outputArrayItem) {
+      if($inputArrayItem == $outputArrayItem) {
+        continue 2;
+      }
+  }
+  $outputArray[] = $inputArrayItem;
+}
+print_r($outputArray);
