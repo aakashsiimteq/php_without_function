778f16f00664bb20376b82af67c6f84e5e2906e5
Aakash Dhar <aakash.dhar@simteq.com>
2018-02-01 12:56:55 +0530

    swap using list

---
+++ swap_witout_temp.php
@@ -0,0 +1,9 @@
+<?php
+
+  $a = 'a stored in $a';
+  $b = 'b stored in $b';
+  echo "Before Swap: \n";
+  echo $a ." - ". $b."\n";
+  list($a,$b) = array($b,$a);
+  echo "After swap: \n";
+  echo $a ." - ".$b;
